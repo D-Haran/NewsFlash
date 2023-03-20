04e83c3df065bbd480b320aaf9774aca1c858752
Derrick <derrickr318@yahoo.com>
2023-03-19 20:02:41 -0400

    changed tab logo

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import '../styles/globals.css'
 import Context from "../context/context"
 import Navbar from '../components/navbar/navbar'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
     <Context>
+    <Head>
+        <link rel="shortcut icon" href="static/LogoOrangeSlim.svg" />
+      </Head>
       <Navbar />
       <Component {...pageProps} />
     </Context>
