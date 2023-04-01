bc1607051a54999df1e80832e1e1c558f2d7fdfc
Derrick <derrickr318@yahoo.com>
2023-04-01 19:00:38 -0400

    added quick seo-config file and added seo tag to app.js

---
+++ next-seo.config.js
@@ -0,0 +1,13 @@
+export default {
+    title: "Newsflash",
+
+    description: "Informed Students, Connected Classrooms.",
+
+    openGraph: {
+        type: "website",
+        locale: "en-IE",
+        url: "http://news-flash.ca",
+        site_name: "NewsFlash"
+    },
+    
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import '../styles/globals.css'
 import Context from "../context/context"
 import Navbar from '../components/navbar/navbar'
+import { DefaultSeo } from 'next-seo'
+
+import SEO from '../next-seo.config'
 
 function MyApp({ Component, pageProps }) {
   return (
     <Context>
+    <DefaultSeo {...SEO} />
       <Navbar />
       <Component {...pageProps} />
     </Context>
